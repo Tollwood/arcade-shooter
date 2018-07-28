2adc663ef7678ee1a8efe0c72fc5aecd2eb82cea
=tollwoo <=tobias.manger@gmail.com>
2018-07-28 20:07:28 +0200

    Floodfill map, keep all fre units accessible

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class MapGenerator : MonoBehaviour {
+
+    public Transform tilePrefab;
+    public Transform obstaclePrefab;
+    public Coord mapSize = new Coord(10,10);
+    [Range(0,1)]
+    public float outlinePercent;
+    public int seed = 1;
+    [Range(0, 1)]
+    public float obstaclePercent;
+
+    List<Coord> allTileCords;
+    Queue<Coord> shuffeledTileCoords;
+    Coord mapCenter;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap(){
+        allTileCords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCords.Add(new Coord(x, y));
+            }
+        }
+        shuffeledTileCoords = new Queue<Coord>(Utility.shuffleArray<Coord>(allTileCords.ToArray(),seed));
+        mapCenter = new Coord((int)mapSize.x/2, (int)mapSize.y/2);
+
+
+        string holderName = "Generated Map";
+
+        if(transform.Find(holderName)){
+            DestroyImmediate(transform.Find(holderName).gameObject);    
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = this.transform;
+        foreach(Coord coord in allTileCords){
+            Transform newTile = Instantiate(tilePrefab, CoordToPosition(coord), Quaternion.Euler(Vector3.right * 90)) as Transform;
+            newTile.localScale = Vector3.one * (1 - outlinePercent);
+            newTile.parent = mapHolder;
+        }
+
+        placeObstacles(mapHolder);
+    }
+
+
+
+    public Vector3 CoordToPosition(Coord coord){
+        return new Vector3(-mapSize.x / 2 + 0.5f + coord.x, 0, -mapSize.y / 2 + 0.5f + coord.y);
+    }
+
+    public Coord GetRandomCoord(){
+        Coord randomCoord = shuffeledTileCoords.Dequeue();
+        shuffeledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+
+    bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
+    {
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(mapCenter);
+        mapFlags[mapCenter.x, mapCenter.y] = true;
+
+        int accessibleTileCount = 1;
+
+        while (queue.Count > 0)
+        {
+            Coord tile = queue.Dequeue();
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    int neighbourX = tile.x + x;
+                    int neighbourY = tile.y + y;
+                    if (x == 0 || y == 0)
+                    {
+                        if (neighbourX >= 0 && neighbourX < obstacleMap.GetLength(0) && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1))
+                        {
+                            if (!mapFlags[neighbourX, neighbourY] && !obstacleMap[neighbourX, neighbourY])
+                            {
+                                mapFlags[neighbourX, neighbourY] = true;
+                                queue.Enqueue(new Coord(neighbourX, neighbourY));
+                                accessibleTileCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int targetAccessibleTileCount = (int)(mapSize.x * mapSize.y - currentObstacleCount);
+        return targetAccessibleTileCount == accessibleTileCount;
+    }
+
+    private void placeObstacles(Transform mapHolder)
+    {
+        bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+
+        int obstacleCount = (int)(mapSize.x * mapSize.y * obstaclePercent);
+        int currentObstacleCount = 0;
+
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            obstacleMap[randomCoord.x, randomCoord.y] = true;
+            currentObstacleCount++;
+
+            if (randomCoord != mapCenter && MapIsFullyAccessible(obstacleMap, currentObstacleCount))
+            {
+                Vector3 obstaclePosition = CoordToPosition(randomCoord);
+                Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * .5f, Quaternion.identity) as Transform;
+                newObstacle.parent = mapHolder;
+            }
+            else
+            {
+                obstacleMap[randomCoord.x, randomCoord.y] = false;
+                currentObstacleCount--;
+            }
+        }
+    }
+
+    public struct Coord {
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public int x;
+        public int y;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Coord))
+                return false;
+
+            Coord coordToEqual = (Coord)obj;
+            return this.x == coordToEqual.x && this.y == coordToEqual.y;
+
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1502939027;
+            hashCode = hashCode * -1521134295 + x.GetHashCode();
+            hashCode = hashCode * -1521134295 + y.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Coord c1, Coord c2)
+        {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1 == c2);
+        }
+    }
+}
